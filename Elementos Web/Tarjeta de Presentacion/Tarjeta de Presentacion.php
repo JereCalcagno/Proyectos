3f28741afc7586a05a5bf4aa903aaed97b22e682
Jere Calcagno <92824956+JereCalcagno@users.noreply.github.com>
2023-03-08 15:01:01 -0300

    Create Tarjeta de Presentacion.php

---
+++ Elementos Web/Tarjeta de Presentacion/Tarjeta de Presentacion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+        <div class="card">
+			<img src="img/JP.png" alt="" class="card-image">
+			<h3 class="card-title">JPerexin</h3>		
+        </div>
+
+</body>
+</html>
